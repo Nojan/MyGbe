ab41f38031cb08d9fd3eea6186909aeadb1d64e7
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2017-07-02 16:26:08 -0400

    bitop

---
+++ DGameBoy/source/bitop.d
@@ -0,0 +1,31 @@
+import numeric_alias;
+
+pure nothrow @nogc
+u8 set(u8 bit, u8 idx) 
+{
+    assert(idx < 8);
+    bit |= 0x1 << idx;
+    return bit;
+}
+
+pure nothrow @nogc
+u8 reset(u8 bit, u8 idx) 
+{
+    assert(idx < 8);
+    bit &= ~(0x1 << idx);
+    return bit;
+}
+
+pure nothrow @nogc
+bool test(u8 bit, u8 idx) 
+{
+    assert(idx < 8);
+    return (bit & (0x1 << idx)) != 0;
+}
+
+pure nothrow @nogc
+int val(u8 bit, u8 idx) 
+{
+    assert(idx < 8);
+    return !!(bit & (0x1 << idx));
+}
